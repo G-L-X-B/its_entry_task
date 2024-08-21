2c688d604a46ea92af47b76fe83a2d538fa507d5
GLXB <ustynow.gleb@gmail.com>
2024-08-22 01:24:00 +0300

    feat: Database history.

--- runtext/urls.py
+++ runtext/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create', views.generate_video, name='create'),
+    path('create', views.create_video, name='create'),
     path('demo', views.serve_demo, name='server_demo'),
 ]
--- runtext/views.py
+++ runtext/views.py
@@ -1,13 +1,17 @@
 from tempfile import NamedTemporaryFile
 
+from django.conf import settings
 from django.http import FileResponse, HttpRequest
 # from django.shortcuts import render
-from django.conf import settings
+
+
+from .models import Generation, Request
 
 from .runtext_engine import create_runtext_videofile
 
 
 def serve_demo(request):
+    Request.objects.create(request_type=Request.DEMO)
     return FileResponse(
         open(settings.BASE_DIR / 'Hello, brotha!.mp4', 'rb'),
         as_attachment=True,
@@ -15,13 +19,17 @@ def serve_demo(request):
     )
 
 
-def generate_video(request: HttpRequest):
+def create_video(request: HttpRequest):
     video_text = request.GET['video_text']
+    opts = {
+        option: request.GET[option]
+        for option in ('duration', 'text_color', 'bg_color')
+        if request.GET.get(option) is not None
+    }
+    gen = Generation.objects.create(text=video_text, **opts)
+    Request.objects.create(request_type=Request.CREATE, generation_options=gen)
     tmpfile = NamedTemporaryFile(buffering=0)
-    create_runtext_videofile(video_text, tmpfile.name,
-                             duration=request.GET.get('duration'),
-                             text_color=request.GET.get('text_color'),
-                             bg_color=request.GET.get('bg_color'))
+    create_runtext_videofile(video_text, tmpfile.name, **opts)
     return FileResponse(
         tmpfile,
         as_attachment=True,
