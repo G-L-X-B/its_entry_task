b260d561cc78758a55708c2ca3618d6cd3495579
GLXB <ustynow.gleb@gmail.com>
2024-08-23 00:50:20 +0300

    fix: Typo fix in url paths.

--- runtext/urls.py
+++ runtext/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create', views.create_video, name='create'),
-    path('demo', views.serve_demo, name='server_demo'),
+    path('demo', views.serve_demo, name='serve_demo'),
 ]
