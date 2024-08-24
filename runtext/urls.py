a22b3c9a5b7825e51f53fd1a30a5270fbb5ddf28
GLXB <ustynow.gleb@gmail.com>
2024-08-25 02:04:40 +0300

    feat: Help page and redirect to it from empty create view.

--- runtext/urls.py
+++ runtext/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create', views.create_video, name='create'),
-    path('demo', views.serve_demo, name='serve_demo'),
+    path('demo', views.serve_demo, name='demo'),
+    path('help', views.help_page, name='help')
 ]
--- runtext/views.py
+++ runtext/views.py
@@ -3,7 +3,7 @@ from urllib.parse import urlencode
 
 from django.conf import settings
 from django.http import FileResponse, HttpRequest
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
 from .models import Generation, Request
@@ -12,23 +12,27 @@ from . import runtext_engine as engine
 from .runtext_engine import create_runtext_videofile
 
 
-def serve_demo(request):
-    Request.objects.create(request_type=Request.DEMO)
-    url = '{}?{}'.format(
-        reverse('create'),
+def _create_default_link(request):
+    return '{}?{}'.format(
+        request.build_absolute_uri(reverse('create')),
         urlencode(dict(
                 video_text='This is a demo runing text video',
                 duration=engine.DEFAULT_DURATION,
                 text_color=engine.DEFAULT_TEXT_COLOR,
-                bg_color=engine.DEFAULT_BG_COLOR
+                bg_color=engine.DEFAULT_BG_COLOR,
             )
         )
     )
-    return redirect(url)
+
+def serve_demo(request):
+    Request.objects.create(request_type=Request.DEMO)
+    return redirect(_create_default_link(request))
 
 
 def create_video(request: HttpRequest):
-    video_text = request.GET['video_text']
+    video_text = request.GET.get('video_text')
+    if video_text is None:
+        return redirect(help_page)
     opts = {
         option: request.GET[option]
         for option in ('duration', 'text_color', 'bg_color')
@@ -43,3 +47,14 @@ def create_video(request: HttpRequest):
         as_attachment=True,
         filename=video_text + '.mp4'
     )
+
+
+def help_page(request: HttpRequest):
+    return render(request, 'runtext/help.html', {
+                    'create': request.build_absolute_uri(reverse('create')),
+                    'default_duration': engine.DEFAULT_DURATION,
+                    'default_text_color': engine.DEFAULT_TEXT_COLOR,
+                    'default_bg_color': engine.DEFAULT_BG_COLOR,
+                    'demo_link': request.build_absolute_uri(reverse('demo')),
+                    'create_demo_link': _create_default_link(request),
+                  })
