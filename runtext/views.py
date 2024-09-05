e2bac3d86b3710dbebdc60c9145c24d539850c24
GLXB <ustynow.gleb@gmail.com>
2024-09-06 01:33:33 +0300

    feat: Input validation.

--- runtext/views.py
+++ runtext/views.py
@@ -39,15 +39,28 @@ def create_video(request: HttpRequest):
         for option in ('duration', 'text_color', 'bg_color')
         if request.GET.get(option) is not None
     }
+    if _check_creation_options(opts) is not None:
+        return redirect(help_page)
     gen = Generation.objects.create(text=video_text, **opts)
     Request.objects.create(request_type=Request.CREATE, generation_options=gen)
     tmpfile = NamedTemporaryFile(buffering=0)
-    create_runtext_videofile(video_text, tmpfile.name, **opts)
+    try:
+        create_runtext_videofile(video_text, tmpfile.name, **opts)
+    except Exception as e:
+        print(f'Error creating video: {e} (request id: {gen.id}).')
+        return redirect(help_page)
     return FileResponse(
         tmpfile,
         as_attachment=True,
         filename=video_text + '.mp4'
     )
+
+
+def _check_creation_options(options: dict):
+    duration = options.get('duration')
+    if duration is None or int(duration) < 1:
+        return 'duration'
+    return None
 
 
 def help_page(request: HttpRequest):
